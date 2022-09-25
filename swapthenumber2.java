9cef6260ca9b36761bc72cc2e79a14ed7846d027
Vishal gore <92011928+vishalgore944@users.noreply.github.com>
2022-09-25 20:29:05 +0530

    Add files via upload

---
+++ swapthenumber2.java
@@ -0,0 +1,13 @@
+public class swapthenumber2 
+{
+    public static void main(String[] args) 
+    {
+        int a=10;
+        int b=12;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);    
+    }    
+}
